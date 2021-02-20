c367ee028a0ccabc48326e2b79e34004813eebcc
tsekeres <twsekeres@gmail.com>
2021-02-19 19:07:36 -0600

    text change

--- src/javascripts/components/bearBuilder.js
+++ src/javascripts/components/bearBuilder.js
@@ -8,7 +8,7 @@ const bearBuilder = (array) => {
                     <img src="${element.imageUrl}" class="card-img-top" alt="Bear in the grass">
                     <div class="card-body">
                     <h5 class="card-title">${element.bearName}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Bears love to fish. We are keeping track of your bear's eating habits and how successful they are at catching fish! Also keep an eye the awards for catches!</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                   </div>`;
