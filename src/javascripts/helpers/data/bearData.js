80ec79feceb3891bf365d6996abb2026e14167d8
tsekeres <twsekeres@gmail.com>
2021-02-23 17:41:22 -0600

    stretch tries

--- src/javascripts/helpers/data/bearData.js
+++ src/javascripts/helpers/data/bearData.js
@@ -18,14 +18,69 @@ const formInput = (e) => {
 
   const bearName = document.querySelector('#bearName').value;
   const imageUrl = document.querySelector('#bearImage').value;
+
+  const bearIds = bearGroup.map((bear) => bear.id).sort((a, b) => a - b);
+  const id = bearIds.length ? bearIds[bearIds.length - 1] + 1 : 1;
+
   const bearsObj = {
     bearName,
     imageUrl,
+    id,
   };
 
   bearGroup.push(bearsObj);
   bearBuilder(bearGroup);
   document.querySelector('#bear-form').reset();
 };
+
+// const attemptFish = (e) => {
+//   const attemptDate = new Date();
+
+//   const targetType = e.target.type;
+//   const targetId = Number(e.target.id);
+
+//   if (targetType === 'button') {
+//     const cardIndex = bearGroup.findIndex(
+//       (bear) => bear.id === targetId
+//     );
+
+//     const attemptsObj = {
+//       attemptDate,
+//     };
+
+//     bearGroup.push(attemptsObj);
+
+//     const fishAttempt = bearGroup.splice(cardIndex, 1);
+//     bearGroup.push(...fishAttempt);
+
+//     cardIndex.parentNode.replaceChild(fishAttempt, cardIndex);
+//     bearBuilder(bearGroup);
+//   }
+// };
+
+// let catches = 0;
+
+// const fishCatch = (e) => {
+//   catches += 1;
+//   const targetType = e.target.type;
+//   const targetId = Number(e.target.id);
+
+//   if (targetType === 'button') {
+//     const cardIndex = bearGroup.findIndex(
+//       (bear) => bear.id === targetId
+//     );
+
+//     const catchObj = {
+//       catches,
+//     };
+
+//     bearGroup.push(catchObj);
+
+//     const catchFish = bearGroup.splice(cardIndex, 1);
+//     bearGroup.push(...catchFish);
+
+//     bearBuilder(bearGroup);
+//   }
+// };
 
 export { formInput, bearGroup };
